6258b2d6bcf24848f6aafadf294a72d3322cec75
Peter Barclay <peter@peter-barclay.com>
2019-10-15 19:45:52 +0100

    Stacking up buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -5,16 +5,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert = false
+    private let countries = [
+        "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",
+        "Nigeria", "Poland", "Russia", "Spain", "UK", "US"
+    ]
+    
+    private var correctAnswer = Int.random(in: 0 ... 2)
     
     var body: some View {
-        Button("Show Alert") {
-            self.showAlert = true
-        }
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("Hello SwiftUI!"),
-                  message: Text("This is some detail message"),
-                  dismissButton: .default(Text("OK")))
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag...")
+                    Text("\(countries[correctAnswer])")
+                }.foregroundColor(.white)
+              
+                ForEach(0 ..< 3) { index in
+                    Button(action: {
+                        // flag was tapped
+                    }) {
+                        Image(self.countries[index])
+                            .renderingMode(.original)
+                    }
+                }
+                
+                Spacer()
+            }
         }
     }
 }
