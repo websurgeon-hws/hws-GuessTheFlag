70a27802a8a358315cfdca286a39f80467c2b87e
Peter Barclay <peter@peter-barclay.com>
2019-12-07 16:24:20 +0000

    Fixing Guess the Flag
    Adding accessibility text to prevent answer being revealed by voice over and providing flag description instead

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -5,6 +5,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var countries = [
         "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",
         "Nigeria", "Poland", "Russia", "Spain", "UK", "US"
@@ -41,7 +55,8 @@ struct ContentView: View {
                         self.flagTapped(index)
                         
                     }) {
-                        FlagImage(named: self.countries[index])
+                        FlagImage(named: self.countries[index],
+                                  label: self.labels[self.countries[index], default: "unknown"])
                     }
                     .rotation3DEffect(
                         .radians(self.animateCorrect[index] ? Double.pi * 2 : 0),
@@ -133,9 +148,11 @@ struct Shake: GeometryEffect {
 
 struct FlagImage: View {
     private let imageName: String
-
-    init(named imageName: String) {
+    private let imageLabel: String
+    
+    init(named imageName: String, label: String) {
         self.imageName = imageName
+        self.imageLabel = label
     }
     
     var body: some View {
@@ -146,6 +163,7 @@ struct FlagImage: View {
             Capsule()
                 .stroke(Color.black, lineWidth: 1))
         .shadow(radius: 2)
+        .accessibility(label: Text(self.imageLabel))
     }
 }
 
